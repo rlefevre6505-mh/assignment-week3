16840d14842d098d2f82a59172a4b55f8a086f94
Richard Lefevre <rlefevre.6505@gmail.com>
2025-12-05 16:57:45 +0000

    feat: added function to apply upgrades when purchased

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+function loadData() {
+  if (localStorage.userData) {
+    userData = JSON.parse(localStorage.getItem("userData"));
+    return userData;
+  } else {
+    const userData = {
+      cookieCount: 0,
+      cps: 0,
+    };
+    return userData;
+  }
+}
+
+userData = loadData();
+
 async function getUpgradeData() {
   const response = await fetch(
     "https://cookie-upgrade-api.vercel.app/api/upgrades"
   );
   const apiData = await response.json();
-  console.log(apiData);
   return apiData;
 }
 
@@ -11,83 +25,46 @@ async function upgradesToPage(apiData) {
   apiData = await getUpgradeData();
   for (let i = 0; i < apiData.length; i++) {
     const newUpgradeBox = document.createElement("p");
-    newUpgradeBox.textContent = `${apiData[i].name}:  ${apiData[i].cost}`;
+    newUpgradeBox.textContent = `${apiData[i].increase} cookies per second  ( COST - ${apiData[i].cost} cookies )`;
     newUpgradeBox.classList.add("upgrade");
     newUpgradeBox.id = i;
     shopContainer.appendChild(newUpgradeBox);
-
-    newUpgradeBox.addEventListener("click", addUpgrade(), {}); //this runs on load, but shouldnt, as it will buy available upgrades automatically when possible!!!!!!!!!!
+    newUpgradeBox.addEventListener("click", function () {
+      addUpgrade(apiData[i]);
+    });
   }
 }
-
-// async function addEvents(apiData) {
-//   apiData = await getUpgradeData();
-//   const button = document.querySelector(".upgrade");
-//   button.addEventlistener("click", addUpgrade());
-// }
-
 const shopContainer = document.getElementById("shop-container");
-
 upgradesToPage(getUpgradeData());
 
-//values to update and replace previous locally stored data
-let cookieCountValue = 0;
-let cpsValue = 0;
-
-//Local Data Storage
-let userData = {
-  cookieCount: cookieCountValue,
-  cps: cpsValue,
-};
-
-//function to save data (every second in loop)
-
 setInterval(function saveAndUpdateDisplay() {
-  // this function is not overwriting the locally saved data (or maybe it is, but the values have not updated?) !!!!!!!!!!!!!!!
+  userData.cookieCount = userData.cookieCount + userData.cps;
   const dataStringy = JSON.stringify(userData);
   localStorage.setItem("userData", dataStringy);
-  console.log("data saved");
   displayScore();
 }, 1000);
 
-console.log(cookieCountValue); //this value is not updating!!!!!!!!!!!!!!!!!!!!
-
-// function saveData(userData) {
-//   const stringifiedUserData = JSON.stringify(userData);
-//   localStorage.setItem("userData", stringifiedUserData);
-// }
-// saveData(userData);
-
-//event listener for cookie click
 const cookienautButton = document.getElementById("cookienaut");
 
 cookienautButton.addEventListener("click", function () {
-  cookieCountValue = cookieCountValue + 1;
+  userData.cookieCount = userData.cookieCount += 1;
   displayScore();
 });
 
-//display score info
 const scoreInfo = document.getElementById("total-cookie-count");
 const cpsInfo = document.getElementById("cookies-per-second");
 
 function displayScore() {
-  scoreInfo.textContent = `Total Cookie Count: ${cookieCountValue}`;
-  cpsInfo.textContent = `Cookies Per Second: ${cpsValue}`;
+  scoreInfo.textContent = `Total Cookie Count: ${userData.cookieCount}`;
+  cpsInfo.textContent = `Cookies Per Second: ${userData.cps}`;
 }
 displayScore();
 
-//use upgrade buttons
-
-const button = document.querySelector(".upgrade");
-
-button.addEventlistener("click", addUpgrade()); //This logs "Uncaught TypeError: Cannot read properties of null (reading 'addEventlistener')"
-
-function addUpgrade() {
-  if (cpsValue >= this.cost) {
-    cookieCountValue = cookieCountValue - this.cost;
-    cpsValue = cpsValue + this.increase;
-    console.log("upgrade button clicked");
+function addUpgrade(item) {
+  if (userData.cookieCount >= item.cost) {
+    userData.cookieCount = userData.cookieCount - item.cost;
+    userData.cps = userData.cps + item.increase;
   } else {
-    console.log("too expensive!");
+    alert("too expensive!");
   }
 }
