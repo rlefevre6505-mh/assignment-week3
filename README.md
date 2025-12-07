5b8f2a2a47d9509c55a1d6de5c528310503c67d6
Richard Lefevre <rlefevre.6505@gmail.com>
2025-12-07 11:32:07 +0000

    docs: updated readme

--- README.md
+++ README.md
@@ -4,9 +4,6 @@ A cookie clicker game using javascript
 
 # Reflections
 
-Please also provide an assignment reflection in your project README.md file.
-
-Required
 🎯 What requirements did you achieve?
 🎯 Were there any requirements or goals that you were unable to achieve?
 🎯 If so, what was it that you found difficult about these tasks?
@@ -14,6 +11,14 @@ Optional
 🏹 Feel free to add any other reflections you would like to share about your submission, for example:
 
 Requesting feedback about a specific part of your submission.
-What useful external sources helped you complete the assignment (e.g Youtube tutorials)?
 What errors or bugs did you encounter while completing your assignment? How did you solve them?
 What went really well and what could have gone better?
+
+For this assignment I achieved all the standard requiredments, and ............ as stretch goals.
+
+I struggled with a few specific aspects of this assignment, despite managing previous assignments quite easily. Firstly, after creating an asunc function to fetch API upgrade data, understanding how tocreate a second async function in which to include the loop for creating upgrade buttons and add event listeners to them. Secondly, getting the game data to store and retreiev properly. Working through these ws a real struggle, even with some guidance from Bertie and Sam, although their help was invaluable! I think the issue with both of these areas was that my understanding of these specific processes and how they can/cannot work was limited, although this has helped me understand them better. Some of the ways I needed to use local storage and API data were outside of what was covered in our workshop, so needed a bit of a deeper understanding, which took a while for me to get, as most of my online research was less halpful than it has been in previous situations, partly due to change of syntax from common JS to ES6.
+
+When starting the assignment, I took phone-first approach to the layout
+
+I decided I wanted the player to have a basic game guide and to have some cosmetic control, so I added a button at the bottom of the screen that creates a new div, covering the game temporarily, in which they can read to understand the concept, choose to have coloured text or white text only, and then exit the pop-up.
+I also considered adding the option for a cleaner font, but did not implement this due to time constraints, although this is definitely somethign I would do for accessibiity purposes.
--- app.js
+++ app.js
@@ -25,12 +25,22 @@ async function upgradesToPage(apiData) {
   apiData = await getUpgradeData();
   for (let i = 0; i < apiData.length; i++) {
     const newUpgradeBox = document.createElement("p");
-    newUpgradeBox.textContent = `${apiData[i].increase} cookies per second  ( COST - ${apiData[i].cost} cookies )`;
+    newUpgradeBox.textContent = `${apiData[i].increase} cookies per second  ( COST - ${apiData[i].cost})`;
     newUpgradeBox.classList.add("upgrade");
     newUpgradeBox.id = i;
     shopContainer.appendChild(newUpgradeBox);
-    newUpgradeBox.addEventListener("click", function () {
-      addUpgrade(apiData[i]);
+    newUpgradeBox.addEventListener("click", function addUpgrade(item) {
+      if (userData.cookieCount >= item.cost) {
+        userData.cookieCount = userData.cookieCount - item.cost;
+        userData.cps = userData.cps + item.increase;
+      } else {
+        const flash = setInterval(function () {
+          newUpgradeBox.animate({ background: "#bd162991" }, { duration: 80 });
+        }, 280);
+        setTimeout(function () {
+          clearInterval(flash);
+        }, 1400);
+      }
     });
   }
 }
@@ -60,11 +70,64 @@ function displayScore() {
 }
 displayScore();
 
-function addUpgrade(item) {
-  if (userData.cookieCount >= item.cost) {
-    userData.cookieCount = userData.cookieCount - item.cost;
-    userData.cps = userData.cps + item.increase;
-  } else {
-    alert("too expensive!");
-  }
-}
+const guideButton = document.getElementById("guide-button");
+
+guideButton.addEventListener("click", function () {
+  const guideBox = document.createElement("div");
+  guideBox.classList.add("guide-box");
+  guideBox.classList.add("font-white");
+  guideBox.textContent = `Cookienaut is on a spacewalk, but the shuttle's onboard oxygen generator has stopped working properly! Help the crew of the shuttle keep hand-cranking the generator... by earning them cookies to eat. Clicking cookienaut generates 1 cookie, and with upgrades available to buy with your collection of cookies, you can automatically generate extra cookies every second!`;
+  document.body.appendChild(guideBox);
+  const emptyDiv = document.createElement("div");
+  emptyDiv.classList.add("empty");
+  guideBox.appendChild(emptyDiv);
+  const optionsDiv = document.createElement("div");
+  optionsDiv.classList.add("options-div");
+  optionsDiv.textContent =
+    "Choose your text colour option for your score info and upgrades:";
+  guideBox.appendChild(optionsDiv);
+  const colourButton = document.createElement("button");
+  const whiteButton = document.createElement("button");
+  colourButton.classList.add("option");
+  colourButton.id = "option-colour";
+  whiteButton.classList.add("option");
+  whiteButton.id = "option-white";
+  whiteButton.textContent = "WHITE";
+  colourButton.textContent = "COLOUR";
+  optionsDiv.appendChild(whiteButton);
+  optionsDiv.appendChild(colourButton);
+
+  colourButton.addEventListener("click", function () {
+    const colour2 = document.getElementById("shop-container");
+    const colour1 = document.getElementById("score-info");
+    colour1.classList.remove("font-white");
+    colour2.classList.remove("font-white");
+    colour1.classList.add("font-colour1");
+    colour2.classList.add("font-colour2");
+  });
+
+  whiteButton.addEventListener("click", function () {
+    const colour2 = document.getElementById("shop-container");
+    const colour1 = document.getElementById("score-info");
+    colour1.classList.remove("font-colour1");
+    colour2.classList.remove("font-colour2");
+    colour1.classList.add("font-white");
+    colour2.classList.add("font-white");
+  });
+
+  //change to exit properly
+  const exitButton = document.createElement("button");
+  exitButton.classList.add("exit-button");
+  exitButton.textContent = "EXIT TO GAME";
+  optionsDiv.appendChild(exitButton);
+  exitButton.addEventListener("click", function () {
+    const openedWindow6 = document.querySelector(".guide-box");
+    openedWindow6.remove();
+  });
+});
+
+// resetButton = document.getElementById("reset");
+// resetButton.addEventListener("click", function () {
+//   userData.cookieCount = 0;
+//   userData.cps = 0;
+// });
